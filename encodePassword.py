4ce4e958f9325d5c11e60efbc50e094d77d8da3c
Ridho Liwardana <76461626+Redoqx@users.noreply.github.com>
2023-10-12 12:41:47 +0700

    Delete encodePassword.py

--- encodePassword.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-def encodePassword(password:str):
-    hash_algorithm = hashlib.sha256()
-    hash_algorithm.update(password.encode())
-    password = hash_algorithm.hexdigest()
-    return password
